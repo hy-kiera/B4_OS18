ab4697b4cda600bb499549bcedd55847e6387a14
hy-kiera <bb.kiera@gmail.com>
2018-05-17 14:04:37 +0900

    first show list

--- list.py
+++ list.py
@@ -1,7 +1,35 @@
 import sqlite3
 
-def list_todo():
+conn = sqlite3.connect("task.db")
+cur = conn.cursor()
+# table col : id, what, due, importance, category, finished
+
+def list_todo_due():
+	slct_data = "select * from todo order by due asc where finished = 1"
+	cur.execute(slct_data)
+	records = cur.fetchall()
+	for row in records:
+		print(row[5], row[3], row[1], row[2], row[4])
 
 def list_todo_importance():
+#	slct_data = "select * from todo where finished = 1"
+#	cur.execute(slct_data)
+	slct_data = "select * from todo order by importance asc, what asc where finished = 1"
+	cur.execute(slct_data)
+	records = cur.fetchall()
+	for row in records:
+		print(row[5], row[3], row[1], row[2], row[4])
 
 def list_todo_input():
+	slct_data = "select * from todo where finished = 1"
+	cur.execute(slct_data)
+	records = cur.fetchall()
+	for row in records:
+		print(row[5], row[3], row[1], row[2], row[4])
+
+def list_todo_category(category):	# 가나다순
+	slct_data = "select * from todo order by due asc where category = ?, finished = 1"
+	cur.execute(slct_data, [category])
+	records = cur.fetchall()
+	for row in records:
+		print(row[5], row[3], row[1], row[2])
