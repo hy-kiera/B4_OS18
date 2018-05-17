d7f19c5e20285c10ad228a20abb10d31672d4e0c
Goodkorning <rnrrhs963@naver.com>
2018-05-17 13:35:26 +0900

    modify 구현

--- modify.py
+++ modify.py
@@ -1,3 +1,13 @@
 import sqlite3
 
-def modify_todo():
+def modify_todo() :
+    conn = sqlite3.connect("lab.db")
+    cur = conn.cursor()
+    mID = input("Record ID?")
+    mTodo = input("Todo?")
+    mDue = input("Due date?")
+    mFinished = input("Finished (1: yes, 0: no)?")
+    cur.execute("""UPDATE todo SET what = ?, due = ?, finished = ? WHERE id = ?""", (mTodo, mDue, mFinished, mID))
+    conn.commit()
+    cur.close()
+    conn.close()
