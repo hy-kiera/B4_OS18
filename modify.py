8dbd226528f339455b2a376b2274a62cb741f593
Ramenseller <moon3545@naver.com>
2018-05-17 13:35:51 +0900

    update

--- modify.py
+++ modify.py
@@ -1,3 +1,17 @@
 import sqlite3
 
 def modify_todo():
+    sql = "update todo set what=?, due=?, category=?, finished=? where id=?"
+
+    id_num = input("Record id? ")
+    what_m = input("Todo? ")
+    due_m = input("Due date? ")
+    category_m = input("Category? ")
+    finished_m = input("Finished (1: yes, 0: no)? ")
+
+    cur.execute(sql, (what_m,due_m,category_m,finished_m,id_num))
+    conn.commit()
+
+    sql = "select * from todo where 1"
+    cur.execute(sql)
+    print("\n")
