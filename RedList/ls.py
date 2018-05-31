7f0973ec98a2e3c08b65aa7c87532082788d9f29
hy-kiera <bb.kiera@gmail.com>
2018-05-31 11:37:56 +0900

    remove list.py

--- RedList/ls.py
+++ RedList/ls.py
@@ -1,5 +1,8 @@
 import sqlite3
 import category as ctg
+from prettytable import PrettyTable
+
+
 import inquirer
 
 conn = sqlite3.connect("task.db")
@@ -10,36 +13,65 @@ def list_todo_due():
 	slct_data = "select * from todo where finished = ? order by due asc, what asc"
 	cur.execute(slct_data,['n'])
 	records = cur.fetchall()
-	for row in records:
-		print(row[5], row[3], row[1], row[2], row[4])
+	
+	    
+	x = PrettyTable()
 
+	x.field_names = ["Finished", "Importance", "What", "due", "category"]
+
+	
+	for row in records:
+		x.add_row([row[5], row[3], row[1], row[2], row[4]])
+	if not len(records) == 0:
+		print(x)
 	print("")
 
 def list_todo_importance():
 	slct_data = "select * from todo where finished = ? order by importance asc, what asc"
 	cur.execute(slct_data,['n'])
 	records = cur.fetchall()
-	for row in records:
-		print(row[5], row[3], row[1], row[2], row[4])
+	    
+	x = PrettyTable()
 
+	x.field_names = ["Finished", "Importance", "What", "due", "category"]
+
+	
+	for row in records:
+		x.add_row([row[5], row[3], row[1], row[2], row[4]])
+	if not len(records) == 0:
+		print(x)
 	print("")
 
 def list_todo_what():
 	slct_data = "select * from todo where finished = ? order by what asc"
 	cur.execute(slct_data,['n'])
 	records = cur.fetchall()
-	for row in records:
-		print(row[5], row[3], row[1], row[2], row[4])
+	    
+	x = PrettyTable()
 
+	x.field_names = ["Finished", "Importance", "What", "due", "category"]
+
+	
+	for row in records:
+		x.add_row([row[5], row[3], row[1], row[2], row[4]])
+	if not len(records) == 0:
+		print(x)
 	print("")
 
 def list_todo_category(category):	# 가나다순
 	slct_data = "select * from todo where category = ? and finished = ? order by category asc"
 	cur.execute(slct_data, [category,'n'])
 	records = cur.fetchall()
-	for row in records:
-		print(row[5], row[3], row[1], row[2])
+	    
+	x = PrettyTable()
 
+	x.field_names = ["Finished", "Importance", "What", "due", "category"]
+
+	
+	for row in records:
+		x.add_row([row[5], row[3], row[1], row[2], row[4]])
+	if not len(records) == 0:
+		print(x)
 	print("")
 
 def list_main():
@@ -60,5 +92,4 @@ def list_main():
 	elif answers['opt'] == 'category':
 		ctg.show_category()
 		c = str(input("What cateogry do you want to list? "))
-
 		list_todo_category(c)
