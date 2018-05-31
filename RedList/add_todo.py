5df8b604e45a8c3fa1045c484ba83f41e0429fb3
hy-kiera <bb.kiera@gmail.com>
2018-05-31 11:52:33 +0900

    add some docstring and info to logo.py

--- RedList/add_todo.py
+++ RedList/add_todo.py
@@ -10,20 +10,6 @@ def add_todo():
 
     sql = "insert into todo (what, due, importance, category, finished) values (?, ?, ?, ?, ?)"
 
-    # lambda를 어떻게 작성해야 할지 모르겠어요.
-    
-    # questions = [
-    #     inquirer.Text('what', message="What?"),
-    #     inquirer.Text('due', message="Due? (yyyy-mm-dd hh:mm:ss)",
-    #             validate=lambda x, _: re.match('\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d', x),
-    #             ),
-    #     inquirer.Text('importance', message="Importance? (1 ~ 5)",
-    #             validate=lambda x, _: re.match('\d', x),
-    #             ),
-    #     inquirer.Text('category', message="Category?"),
-    #     ]
-    # answers = inquirer.prompt(questions)
-
     while True:
         what = str(input("What? "))
         if what != '':
@@ -53,7 +39,6 @@ def add_todo():
     if category == '':
         category = 'GENERAL'
 
-    # data = [answers['what'], answers['due'], int(answers['importance']), answers['category'], "n"]
     data = [what, due, importance, category, "n"]
 
     cur.execute(sql, data)
