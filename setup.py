931b65d538d83aec80525503045dc5be15e1f2bc
hy-kiera <bb.kiera@gmail.com>
2018-06-14 10:05:58 +0900

    delete nargs in parser option

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'redlist',
-    version = '0.0.6',
+    version = '0.0.7',
     description = 'A simple schedule managing cli app',
     long_description = long_description,
     long_description_content_type='text/markdown',
